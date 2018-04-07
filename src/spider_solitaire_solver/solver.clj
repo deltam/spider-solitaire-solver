174f3cbc54e96a03b87e486261c54442de439bc9
MISUMI Masaru <deltam@gmail.com>
2018-04-07 22:08:11 +0900

    Added generate valid-move func

--- src/spider_solitaire_solver/solver.clj
+++ src/spider_solitaire_solver/solver.clj
@@ -1,15 +1,34 @@
 (ns spider-solitaire-solver.solver
+  "スパイダーソリティアを解く"
   (:require [spider-solitaire-solver.core :as ss]))
 
-(defn move-list
+(defn count-movable-card
+  "山の移動可能なカードを数える"
+  [stk]
+  (count
+   (filter ss/open-card? stk)))
+
+(defn all-move
+  "可能なカード移動のパターンをすべて返す"
+  [deck]
+  (apply concat
+         (for [from (range 10), to (range 10)
+               :let [limit (count-movable-card (nth (:stacks deck) from))]
+               :when (not= from to)]
+           (map #(vector false from to %)
+                (filter (fn [n] (ss/movable? deck from to n))
+                        (range 1 (inc limit))))
+           )))
+
+(defn valid-move
   "可能な手のリストを返す"
   [deck]
-  (cons
-   (if (ss/drawable? deck)
-     [true 0 0 0])
-   [[false 0 1 1]]))
+  (let [moves (all-move deck)]
+    (if (ss/drawable? deck)
+      (conj moves [true 0 0 0])
+      moves)))
 
-(defn eval-deck
+(defn score
   "デッキの評価値を返す"
   [deck]
   1)
